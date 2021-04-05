85ca85850eee39342b2e494ca3e052ef69f0b0d1
Andrei Matveiakin <a.matveiakin@gmail.com>
2021-04-05 18:06:00 +0300

    Add scoped profiler

--- cpp/lib/profiler.h
+++ cpp/lib/profiler.h
@@ -5,19 +5,33 @@
 #include <string>
 
 
+// TODO: Rename to `Timer`.
 class Profiler {
 public:
   Profiler() {
     start_ = std::chrono::steady_clock::now();
   }
 
-  void finish(const std::string& phase_name) {
+  void finish(const std::string& operation) {
     const auto finish = std::chrono::steady_clock::now();
     const double time_sec = (finish - start_) / std::chrono::milliseconds(1) / 1000.;
-    std::cout << "Profiler: " << phase_name << " took " << time_sec << " seconds\n";
+    std::cout << "Profiler: " << operation << " took " << time_sec << " seconds\n";
     start_ = finish;
   }
 
 private:
   std::chrono::time_point<std::chrono::steady_clock> start_;
+};
+
+
+class ScopedProfiler {
+public:
+  ScopedProfiler(std::string operation) : operation_(std::move(operation)) {}
+  ~ScopedProfiler() {
+    profiler_.finish(operation_);
+  }
+
+private:
+  Profiler profiler_;
+  std::string operation_;
 };
