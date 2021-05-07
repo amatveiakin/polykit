2bcde9ddcd55565f2a08d70ec168c9f917a9d55b
Andrei Matveiakin <a.matveyakin@gmail.com>
2021-05-07 16:38:56 +0200

    Split test compilation caches via platform_suffix

--- cpp/test_extra.sh
+++ cpp/test_extra.sh
@@ -1,1 +1,1 @@
-bazel test -c opt --config=clang --cxxopt=-DRUN_LARGE_TESTS --test_output=errors :all_tests
+bazel test -c opt --config=clang --platform_suffix=test_extra --cxxopt=-DRUN_LARGE_TESTS --test_output=errors :all_tests
