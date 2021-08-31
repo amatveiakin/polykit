c03c30e6dc74412593362b485b4e822ba79a4a73
Andrei Matveiakin <a.matveiakin@gmail.com>
2021-08-31 13:31:25 +0300

    Remove duplicate rows first in rank tool

--- pybind/rank.py
+++ pybind/rank.py
@@ -7,7 +7,25 @@ parser.add_argument('filename')
 args = parser.parse_args()
 
 mat_sparse = scipy.sparse.load_npz(args.filename)
-mat = mat_sparse.toarray()
 print('matrix loaded')
+
+# mat = mat_sparse.toarray()
+# print('dense built')
+# rank = np.linalg.matrix_rank(mat)
+# print(f'{mat.shape} => {rank}')
+
+unique_rows = set()
+mat_lil = mat_sparse.transpose().tolil()
+print(f'LIL built')
+for row, value in zip(mat_lil.rows, mat_lil.data):
+    unique_rows.add(tuple(zip(row, value)))
+print(f'{len(unique_rows)} / {len(mat_lil.rows)}')
+print('unique rows built')
+
+mat = np.zeros((len(unique_rows), mat_lil.shape[1]))
+for row in unique_rows:
+    for col, value in row:
+        mat[row, col] = value
+print('dense built')
 rank = np.linalg.matrix_rank(mat)
 print(f'{mat.shape} => {rank}')
--- pybind/stats.py
+++ pybind/stats.py
@@ -9,4 +9,23 @@ args = parser.parse_args()
 mat_sparse = scipy.sparse.load_npz(args.filename)
 bytes_per_int = 4
 ram_estimate = mat_sparse.shape[0] * mat_sparse.shape[1] * bytes_per_int
-print(f'{mat_sparse.shape} ~ {ram_estimate:,}')
+print(f'{mat_sparse.shape} ~ {ram_estimate:,}B as dense')
+
+# mat_coo = mat_sparse.tocoo()
+# for row, col, value in zip(mat_coo.row, mat_coo.col, mat_coo.data):
+
+# i = 0
+# for row in mat_sparse.transpose().tocsr():
+# for row in mat_sparse.transpose().tolil():
+#     print(row)
+#     print('---')
+#     i += 1
+#     if i > 100:
+#         break
+
+unique_rows = set()
+mat_lil = mat_sparse.transpose().tolil()
+print(f'LIL built')
+for row, value in zip(mat_lil.rows, mat_lil.data):
+    unique_rows.add(tuple(zip(row, value)))
+print(f'{len(unique_rows)} / {len(mat_lil.rows)}')
