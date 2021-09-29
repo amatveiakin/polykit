808c6e8611a3c70c7d2592161dc78e00d9dc43f8
Andrei Matveiakin <a.matveiakin@gmail.com>
2021-09-29 16:14:48 +0300

    Add APPLY function wrapper

--- cpp/lib/macros.h
+++ cpp/lib/macros.h
@@ -4,5 +4,11 @@
 #define INTERNAL_STRINGIFY(s) #s
 #define STRINGIFY(s) INTERNAL_STRINGIFY(s)
 
+// TODO: Move `DISAMBIGUATE` and `APPLY` to an appropriate header.
 // Automatically chooses function overload depending on the context.
-#define DISAMBIGUATE(func)  [](auto... args) { return func(args...); }
+#define DISAMBIGUATE(func)  \
+    [](auto&&... args) { return func(std::forward<decltype(args)>(args)...); }
+
+// Wraps a function accepting multiple arguments to accept a tuple.
+#define APPLY(func)  \
+    [](auto&& tuple) { return std::apply(func, std::forward<decltype(tuple)>(tuple)); }
