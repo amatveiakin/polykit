881b09ced6de86b17d1c5a228bd46be1ede5d147
Andrei Matveiakin <a.matveyakin@gmail.com>
2022-02-05 19:39:54 +0300

    Clean up workspace

--- cpp/workspace.cpp
+++ cpp/workspace.cpp
@@ -130,33 +130,6 @@ GammaExpr SymmCGrLi4(const std::vector<int>& points) {
   );
 }
 
-GammaNCoExpr C33(const std::vector<int>& points) {
-  CHECK_EQ(points.size(), 4);
-  const auto pl = [&](int p1, int p2, int p3) {
-    return G(choose_indices_one_based(points, {p1,p2,p3}));
-  };
-  return
-    + ncoproduct(pl(1,2,3), pl(1,2,4), pl(1,3,4))
-    - ncoproduct(pl(2,3,4), pl(2,3,1), pl(2,4,1))
-    + ncoproduct(pl(3,4,1), pl(3,4,2), pl(3,1,2))
-    - ncoproduct(pl(4,1,2), pl(4,1,3), pl(4,2,3))
-  ;
-}
-
-GammaNCoExpr C335(const std::vector<int>& points) {
-  CHECK_EQ(points.size(), 5);
-  const auto c = [&](int p1, int p2, int p3, int p4) {
-    return C33(choose_indices_one_based(points, {p1,p2,p3,p4}));
-  };
-  return
-    + c(1,2,3,4)
-    + c(2,3,4,5)
-    + c(3,4,5,1)
-    + c(4,5,1,2)
-    + c(5,1,2,3)
-  ;
-}
-
 // TODO: rename
 GammaExpr arrow_left(const GammaExpr& expr, int num_dst_points) {
   GammaExpr ret;
