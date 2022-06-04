afdedfd47389e8ba501f4bea1e04d652d7a858fc
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-06-04 20:43:13 +0800

    WIP. Pass shuffle arguments by reference
    The argument is moved only in rare trivial cases (like one word

--- rust/src/math/shuffle.rs
+++ rust/src/math/shuffle.rs
@@ -3,7 +3,7 @@ use crate::math::{Linear, LinearMonom};
 
 
 // TODO: Unroll
-pub fn shuffle_product<T, MonomT>(u: MonomT, v: MonomT) -> Linear<MonomT>
+pub fn shuffle_product<T, MonomT>(u: &MonomT, v: &MonomT) -> Linear<MonomT>
 where
     T: Clone,
     MonomT: LinearMonom + VectorLike<T>,
@@ -12,21 +12,21 @@ where
         return Linear::<MonomT>::zero();
     }
     if u.is_empty() {
-        return Linear::<MonomT>::single(v);
+        return Linear::<MonomT>::single(v.clone());
     }
     if v.is_empty() {
-        return Linear::<MonomT>::single(u);
+        return Linear::<MonomT>::single(u.clone());
     }
     let mut u_trunc = u.clone();
     let mut v_trunc = v.clone();
     let a: T = u_trunc.pop().unwrap();
     let b: T = v_trunc.pop().unwrap();
     let mut ret = Linear::<MonomT>::zero();
-    for (mut w, coeff) in shuffle_product(u, v_trunc).into_iter() {
+    for (mut w, coeff) in shuffle_product(u, &v_trunc).into_iter() {
         w.push(b.clone());
         ret.add_to(w, coeff);
     }
-    for (mut w, coeff) in shuffle_product(u_trunc, v).into_iter() {
+    for (mut w, coeff) in shuffle_product(&u_trunc, v).into_iter() {
         w.push(a.clone());
         ret.add_to(w, coeff);
     }
@@ -42,12 +42,12 @@ where
     match words.len() {
         0 => Linear::<MonomT>::zero(),
         1 => Linear::<MonomT>::single(words[0].clone()),
-        2 => shuffle_product(words[0].clone(), words[1].clone()),
+        2 => shuffle_product(&words[0], &words[1]),
         _ => {
             let w_tail = words.last().unwrap();
             let shuffle_product_head = shuffle_product_multi(&words[0 .. words.len()-1]);
             shuffle_product_head.map_expanding(|w_head| {
-                shuffle_product(w_head.clone(), w_tail.clone())
+                shuffle_product(&w_head, w_tail)
             })
         },
     }
