06f5c2897a65a2b4555245bb7207d0187cd61b92
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-06-21 10:00:11 +0800

    Factor out CGrLi equations into tests

--- cpp/test/polylog_cgrli_test.cpp
+++ cpp/test/polylog_cgrli_test.cpp
@@ -5,6 +5,7 @@
 #include "lib/itertools.h"
 #include "lib/polylog_grqli.h"
 #include "lib/polylog_gr_space.h"
+#include "lib/polylog_qli.h"
 #include "lib/zip.h"
 #include "test_util/matchers.h"
 #include "test_util/space_matchers.h"
@@ -32,6 +33,11 @@ auto sum_alternating(const F& f, const std::vector<int>& points, const std::vect
     ret += sign * f(apply_permutation(alternating, p, points));
   }
   return ret;
+}
+
+GammaExpr AomotoPolylog(const std::vector<int>& points) {
+  const int weight = div_int(points.size(), 2) - 1;
+  return CGrLiVec(weight, points);
 }
 
 GammaExpr CasimirDim3(const std::vector<int>& points) {
@@ -151,6 +157,15 @@ TEST(CGrLiTest, IsTotallyWeaklySeparated) {
   }
 }
 
+TEST(CGrLiTest, CGrLiOfFourPointsIsQLi) {
+  for (const int weight : range_incl(2, 5)) {
+    EXPECT_EXPR_EQ_AFTER_LYNDON(
+      CGrLiVec(weight, {1,2,3,4}),
+      delta_expr_to_gamma_expr(QLiVec(weight, {1,2,3,4}))
+    );
+  }
+}
+
 TEST(CGrLiTest, EqualsAlternative_Dim3) {
   for (const int weight : range_incl(2, 5)) {
     // TODO: Sync indices
@@ -171,6 +186,55 @@ TEST(CGrLiTest, SymmCGrLiIsSymmetric) {
     + SymmCGrLi3({1,2,3,4,5,6})
     + SymmCGrLi3({2,3,4,5,6,1})
   );
+}
+
+TEST(CGrLiTest, LARGE_AomotoPolylogProperties) {
+  // First: any permutation of (1..n) and any permutation of (n+1..2n) multiplies the function
+  // by permutation sign.
+  // For (1,2,3) and (5,6,7) it's trivial from definition, so checking the rest:
+  EXPECT_EXPR_ZERO_AFTER_LYNDON(
+    + CGrLi3(1,2,3,4,5,6,7,8)
+    + CGrLi3(1,2,4,3,5,6,7,8)
+  );
+  EXPECT_EXPR_ZERO_AFTER_LYNDON(
+    + CGrLi3(1,2,3,4,5,6,7,8)
+    + CGrLi3(1,2,3,4,5,6,8,7)
+  );
+  // Second: swap (1..n) and (n+1..2n).
+  EXPECT_EXPR_ZERO_AFTER_LYNDON(
+    + CGrLi3(1,2,3,4,5,6,7,8)
+    - CGrLi3(5,6,7,8,1,2,3,4)
+  );
+  EXPECT_EXPR_ZERO_AFTER_LYNDON(
+    + CGrLi4(1,2,3,4,5,6,7,8,9,10)
+    + CGrLi4(6,7,8,9,10,1,2,3,4,5)
+  );
+  // Third:
+  EXPECT_EXPR_ZERO_AFTER_LYNDON(
+    + CGrLi3(1,2,3,4,6,7,8,9)
+    - CGrLi3(1,2,3,5,6,7,8,9)
+    + CGrLi3(1,2,4,5,6,7,8,9)
+    - CGrLi3(1,3,4,5,6,7,8,9)
+    + CGrLi3(2,3,4,5,6,7,8,9)
+  );
+}
+
+TEST(CGrLiTest, CGrLiViaLowerNumberOfPoints) {
+  // Should be true for any weight
+  for (const int weight : range_incl(2, 3)) {
+    const int p = weight + 1;
+    const int num_points = 2 * p;
+    const auto points = to_vector(range_incl(1, num_points));
+    const auto lhs = CGrLiVec(weight, points);
+    GammaExpr rhs;
+    for (const int i : range(p)) {
+      for (const int j : range(p, 2 * p)) {
+        const int sign = neg_one_pow(points[i] + points[j]);
+        rhs += sign * CGrLiVec(weight, {points[j]}, removed_indices(points, {i, j}));
+      }
+    }
+    EXPECT_EXPR_EQ_AFTER_LYNDON(lhs, neg_one_pow(weight) * rhs);
+  }
 }
 
 TEST(CGrLiTest, CGrLi3Equation) {
