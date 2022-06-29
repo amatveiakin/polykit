c140d45e0aa7df68241675606a2f1c6c222e09b0
Andrei Matveiakin <a.matveiakin@gmail.com>
2022-06-29 10:42:28 +0800

    Short notation for CGrLi with pullback

--- cpp/lib/polylog_cgrli.h
+++ cpp/lib/polylog_cgrli.h
@@ -5,7 +5,6 @@
 GammaExpr CGrLiVec(int weight, const std::vector<int>& points);
 
 // Equivalent to `pullback(CGrLiVec(weight, li_points), pb_points)`
-// TODO: Short notation for this.
 GammaExpr CGrLiVec(int weight, const std::vector<int>& pb_points, const std::vector<int>& li_points);
 
 GammaExpr SymmCGrLi3(const std::vector<int>& points);
@@ -17,17 +16,43 @@ bool are_CGrLi_args_ok(int weight, int num_points);
 
 
 namespace internal {
-template<typename... Args>
-GammaExpr CGrLi_dispatch(int weight, Args... args) {
-  return CGrLiVec(weight, {args...});
-}
+struct CGrLiFixedWeightAndPbPoints {
+  CGrLiFixedWeightAndPbPoints(int weight, std::vector<int> pb_points)
+    : weight_(weight), pb_points_(std::move(pb_points)) {}
+
+  template<typename... Args>
+  GammaExpr operator()(Args... args) {
+    return CGrLiVec(weight_, pb_points_, {args...});
+  }
+private:
+  int weight_ = 0;
+  std::vector<int> pb_points_;
+};
+
+struct CGrLiFixedWeight {
+  CGrLiFixedWeight(int weight) : weight_(weight) {}
+
+  template<typename... Args>
+  GammaExpr operator()(Args... args) {
+    return CGrLiVec(weight_, {args...});
+  }
+
+  template<typename... Args>
+  auto operator[](std::vector<int> args) {
+    return CGrLiFixedWeightAndPbPoints(weight_, std::move(args));
+  }
+private:
+  int weight_ = 0;
+};
 }  // namespace internal
 
-template<typename... Args> GammaExpr CGrLi1(Args... args) { return internal::CGrLi_dispatch(1, args...); }
-template<typename... Args> GammaExpr CGrLi2(Args... args) { return internal::CGrLi_dispatch(2, args...); }
-template<typename... Args> GammaExpr CGrLi3(Args... args) { return internal::CGrLi_dispatch(3, args...); }
-template<typename... Args> GammaExpr CGrLi4(Args... args) { return internal::CGrLi_dispatch(4, args...); }
-template<typename... Args> GammaExpr CGrLi5(Args... args) { return internal::CGrLi_dispatch(5, args...); }
-template<typename... Args> GammaExpr CGrLi6(Args... args) { return internal::CGrLi_dispatch(6, args...); }
-template<typename... Args> GammaExpr CGrLi7(Args... args) { return internal::CGrLi_dispatch(7, args...); }
-template<typename... Args> GammaExpr CGrLi8(Args... args) { return internal::CGrLi_dispatch(8, args...); }
+// TODO: Less clumsy pullback points notations
+// TODO: Synchronize pullback points notation with GrLog and GrQLi
+inline static auto CGrLi1 = internal::CGrLiFixedWeight(1);
+inline static auto CGrLi2 = internal::CGrLiFixedWeight(2);
+inline static auto CGrLi3 = internal::CGrLiFixedWeight(3);
+inline static auto CGrLi4 = internal::CGrLiFixedWeight(4);
+inline static auto CGrLi5 = internal::CGrLiFixedWeight(5);
+inline static auto CGrLi6 = internal::CGrLiFixedWeight(6);
+inline static auto CGrLi7 = internal::CGrLiFixedWeight(7);
+inline static auto CGrLi8 = internal::CGrLiFixedWeight(8);
