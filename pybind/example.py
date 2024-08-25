17f9a81759db615b39b718c1c7b7c27817c28150
Andrei Matveiakin <a.matveiakin@gmail.com>
2024-08-25 16:10:17 +0400

    Migrate from WORKSPACE to MODULE.bazel
    https://bazel.build/external/migration

--- pybind/example.py
+++ pybind/example.py
@@ -10,25 +10,26 @@ import time
 from python.polypy.lib.linear import Linear
 from python.polypy.lib.profiler import Profiler
 from python.polypy.lib.util import rotate_list, to_hashable
-from delta_matrix import DeltaExprMatrixBuilder
 
-from polykit import Encoder, RichTextFormat, AnnotationSorting, set_formatting, reset_formatting, NoLineLimit
-from polykit import tensor_product, to_lyndon_basis
-from polykit import icoproduct, ncoproduct, icomultiply, ncomultiply
-from polykit import X, Inf, Zero, x1, x2, x3, x4, x5, x6, x7, x8, x1s, x2s, x3s, x4s, x5s, x6s, x7s, x8s
-from polykit import Delta, DeltaExpr, substitute_variables_0_based, substitute_variables_1_based, involute
-from polykit import ProjectionExpr, project_on
-from polykit import terms_with_num_distinct_variables, terms_with_min_distinct_variables, terms_containing_only_variables, terms_without_variables
-from polykit import sorted_by_num_distinct_variables
-from polykit import CrossRatio, CompoundRatio, CR
-from polykit import I, Corr
-from polykit import QLi, QLi1, QLi2, QLi3, QLi4, QLi5, QLi6, QLi7, QLi8
-from polykit import QLiNeg, QLiNeg1, QLiNeg2, QLiNeg3, QLiNeg4, QLiNeg5, QLiNeg6, QLiNeg7, QLiNeg8
-from polykit import QLiSymm, QLiSymm1, QLiSymm2, QLiSymm3, QLiSymm4, QLiSymm5, QLiSymm6, QLiSymm7, QLiSymm8
-from polykit import Log, A2
-from polykit import QLiPr
-from polykit import Lira, Lira0, Lira1, Lira2, Lira3, Lira4, Lira5, Lira6, Lira7, Lira8
-from polykit import project_on, project_on_x1, project_on_x2, project_on_x3, project_on_x4, project_on_x5, project_on_x6, project_on_x7, project_on_x8, project_on_x9, project_on_x10, project_on_x11, project_on_x12, project_on_x13, project_on_x14, project_on_x15
+from pybind.delta_matrix import DeltaExprMatrixBuilder
+
+from pybind.polykit import Encoder, RichTextFormat, AnnotationSorting, set_formatting, reset_formatting, NoLineLimit
+from pybind.polykit import tensor_product, to_lyndon_basis
+from pybind.polykit import icoproduct, ncoproduct, icomultiply, ncomultiply
+from pybind.polykit import X, Inf, Zero, x1, x2, x3, x4, x5, x6, x7, x8, x1s, x2s, x3s, x4s, x5s, x6s, x7s, x8s
+from pybind.polykit import Delta, DeltaExpr, substitute_variables_0_based, substitute_variables_1_based, involute
+from pybind.polykit import ProjectionExpr, project_on
+from pybind.polykit import terms_with_num_distinct_variables, terms_with_min_distinct_variables, terms_containing_only_variables, terms_without_variables
+from pybind.polykit import sorted_by_num_distinct_variables
+from pybind.polykit import CrossRatio, CompoundRatio, CR
+from pybind.polykit import I, Corr
+from pybind.polykit import QLi, QLi1, QLi2, QLi3, QLi4, QLi5, QLi6, QLi7, QLi8
+from pybind.polykit import QLiNeg, QLiNeg1, QLiNeg2, QLiNeg3, QLiNeg4, QLiNeg5, QLiNeg6, QLiNeg7, QLiNeg8
+from pybind.polykit import QLiSymm, QLiSymm1, QLiSymm2, QLiSymm3, QLiSymm4, QLiSymm5, QLiSymm6, QLiSymm7, QLiSymm8
+from pybind.polykit import Log, A2
+from pybind.polykit import QLiPr
+from pybind.polykit import Lira, Lira0, Lira1, Lira2, Lira3, Lira4, Lira5, Lira6, Lira7, Lira8
+from pybind.polykit import project_on, project_on_x1, project_on_x2, project_on_x3, project_on_x4, project_on_x5, project_on_x6, project_on_x7, project_on_x8, project_on_x9, project_on_x10, project_on_x11, project_on_x12, project_on_x13, project_on_x14, project_on_x15
 
 
 set_formatting(
